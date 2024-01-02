f98603caa6ae6fd45fcc39ff7e51656b0e8032bf
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-01-02 14:50:13 -0300

    feat(authentication): set authentication route

---
+++ functions/routes/bling/authentication.js
@@ -0,0 +1,100 @@
+const getAppData = require('./../../lib/store-api/get-app-data')
+const updateAppData = require('./../../lib/store-api/update-app-data')
+
+exports.post = ({ appSdk, admin }, req, res) => {
+  console.log('>> POST  BLING')
+  const { body, query } = req
+  const { state, code } = query
+  const storeId = parseInt(state, 10)
+  console.log('>> Store: ', storeId, ' code: ', code, ' <<')
+  if (storeId > 100 && code) {
+    return appSdk.getAuth(storeId)
+      .then(async (auth) => {
+        try {
+          await updateAppData({ appSdk, storeId, auth }, {
+            code
+          })
+          res.status(200).redirect('https://app.e-com.plus/#/apps/edit/102418/')
+        } catch (error) {
+          console.error(error)
+          const { response, config } = error
+          let status
+          if (response) {
+            status = response.status
+            const err = new Error(`#${storeId} Bling Webhook error ${status}`)
+            err.url = config && config.url
+            err.status = status
+            err.response = JSON.stringify(response.data)
+            console.error(err)
+          }
+          if (!res.headersSent) {
+            res.send({
+              status: status || 500,
+              msg: `#${storeId} Bling Webhook error`
+            })
+          }
+        }
+      })
+      .catch(() => {
+        console.log('Unauthorized')
+        if (!res.headersSent) {
+          res.sendStatus(401)
+        }
+      })
+    res.status(200)
+  } else {
+    return res.send({
+      status: 404,
+      msg: `StoreId #${storeId} not found`
+    })
+  }
+}
+
+exports.get = ({ appSdk, admin }, req, res) => {
+  console.log('>> POST  BLING')
+  const { body, query } = req
+  const { state, code } = query
+  const storeId = parseInt(state, 10)
+  console.log('>> Store: ', storeId, ' code: ', code, ' <<')
+  if (storeId > 100 && code) {
+    return appSdk.getAuth(storeId)
+      .then(async (auth) => {
+        try {
+          await updateAppData({ appSdk, storeId, auth }, {
+            code
+          })
+          res.status(200).redirect('https://app.e-com.plus/#/apps/edit/102418/')
+        } catch (error) {
+          console.error(error)
+          const { response, config } = error
+          let status
+          if (response) {
+            status = response.status
+            const err = new Error(`#${storeId} Bling Webhook error ${status}`)
+            err.url = config && config.url
+            err.status = status
+            err.response = JSON.stringify(response.data)
+            console.error(err)
+          }
+          if (!res.headersSent) {
+            res.send({
+              status: status || 500,
+              msg: `#${storeId} Bling Webhook error`
+            })
+          }
+        }
+      })
+      .catch(() => {
+        console.log('Unauthorized')
+        if (!res.headersSent) {
+          res.sendStatus(401)
+        }
+      })
+    res.status(200)
+  } else {
+    return res.send({
+      status: 404,
+      msg: `StoreId #${storeId} not found`
+    })
+  }
+}
