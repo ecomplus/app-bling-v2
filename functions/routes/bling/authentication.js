ef56451f823920cfdb790872d80a78a91ec35a67
matheusgnreis <matheus@e-com.club>
2024-01-15 13:37:19 -0300

    chore: set log before authentication

--- functions/routes/bling/authentication.js
+++ functions/routes/bling/authentication.js
@@ -13,13 +13,20 @@ exports.post = async ({ appSdk, admin }, req, res) => {
     return appSdk.getAuth(storeId)
       .then(async (auth) => {
         try {
-          getAppData({ appSdk, storeId, auth })
+          return getAppData({ appSdk, storeId, auth })
             .then(appData => {
               const { client_id, client_secret } = appData
+              console.log('Pass variables', JSON.stringify({client_id, client_secret, code, storeId}))
               const bling = new Bling(client_id, client_secret, code, storeId)
-              setTimeout(() => {
-                res.status(200).redirect('https://app.e-com.plus/#/apps/edit/102418/')
-              }, 1000)
+              return bling.preparing
+                .then((e) => {
+                  console.log('deu certo a criação de autenticação')
+                  res.status(200).redirect('https://app.e-com.plus/#/apps/edit/102418/')
+                })
+                .catch(err => {
+                  console.log(err)
+                  res.status(200).redirect('https://app.e-com.plus/#/apps/edit/102418/')
+                })
             })
         } catch (error) {
           console.error(error)
