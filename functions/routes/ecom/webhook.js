79754c96dc398dabaa01dfcd8b450df037eddd4e
matheusgnreis <matheus@e-com.club>
2024-01-22 13:07:29 -0300

    chore: teste webhook route

--- functions/routes/ecom/webhook.js
+++ functions/routes/ecom/webhook.js
@@ -29,7 +29,7 @@ const removeFromQueue = (resourceId) => {
 exports.post = async ({ appSdk, admin }, req, res) => {
   // receiving notification from Store API
   const { storeId } = req
-  console.log('acionando webhook', storeId)
+  console.log('acionando webhook', storeId, JSON.stringify(req.body))
   if (req.get('host') && !baseUri.includes(req.get('host'))) {
     console.log('>>> Proxy to function v2')
     const axios = require('axios')
