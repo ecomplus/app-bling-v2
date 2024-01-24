e2d729f9ca0156630257a29de2ec4a63badcdabf
matheusgnreis <matheus@e-com.club>
2024-01-24 14:18:41 -0300

    chore: set procedure to self edit

--- functions/routes/ecom/auth-callback.js
+++ functions/routes/ecom/auth-callback.js
@@ -31,16 +31,13 @@ exports.post = ({ appSdk }, req, res) => {
           if (!row.setted_up) {
             console.log(`Try saving procedures for store #${storeId}`)
 
-            /**
-             * You may want to be notified when app "self" data is edited:
-
             procedures[0].triggers.push({
               resource: 'applications',
               resource_id: row.application_id,
               field: 'data'
             })
 
-             */
+            
 
             // must save procedures once only
             return appSdk.saveProcedures(storeId, procedures, auth)
