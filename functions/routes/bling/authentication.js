8db8023f225b05429a8b46c76b4e14f9695c25e6
matheusgnreis <matheus@e-com.club>
2024-01-25 01:03:40 -0300

    chore: change variables names

--- functions/routes/bling/authentication.js
+++ functions/routes/bling/authentication.js
@@ -1,59 +1,5 @@
 const getAppData = require('./../../lib/store-api/get-app-data')
-const Bling = require('../../lib/bling-auth/create-access')
-
-exports.post = async ({ appSdk, admin }, req, res) => {
-  console.log('>> POST  BLING')
-  const { body, query } = req
-  const { state, code } = query
-  console.log('Query', JSON.stringify(query))
-  const storeId = parseInt(query.storeId, 10)
-  console.log('>> Store: ', storeId, ' code: ', code, '<<')
-  if (storeId > 100 && code) {
-    return appSdk.getAuth(storeId)
-      .then(async (auth) => {
-        try {
-          getAppData({ appSdk, storeId, auth })
-            .then(async appData => {
-              const { client_id, client_secret } = appData
-              console.log('Pass variables', JSON.stringify({client_id, client_secret, code, storeId}))
-              await new Bling(client_id, client_secret, code, storeId)
-              setTimeout(() => {
-                return res.status(200).redirect('https://app.e-com.plus/#/apps/edit/102418/')
-              }, 2000)
-            })
-        } catch (error) {
-          console.error(error)
-          const { response, config } = error
-          let status
-          if (response) {
-            status = response.status
-            const err = new Error(`#${storeId} Bling Webhook error ${status}`)
-            err.url = config && config.url
-            err.status = status
-            err.response = JSON.stringify(response.data)
-            console.error(err)
-          }
-          if (!res.headersSent) {
-            res.send({
-              status: status || 500,
-              msg: `#${storeId} Bling Webhook error`
-            })
-          }
-        }
-      })
-      .catch(() => {
-        console.log('Unauthorized')
-        if (!res.headersSent) {
-          res.sendStatus(401)
-        }
-      })
-  } else {
-    return res.send({
-      status: 404,
-      msg: `StoreId #${storeId} not found`
-    })
-  }
-}
+const blingAuth = require('../../lib/bling-auth/create-access')
 
 exports.get = async ({ appSdk, admin }, req, res) => {
   console.log('>> GET  BLING')
@@ -69,7 +15,7 @@ exports.get = async ({ appSdk, admin }, req, res) => {
             .then(async appData => {
               const { client_id, client_secret } = appData
               console.log('Pass variables', JSON.stringify({client_id, client_secret, code, storeId}))
-              const bling = await bling(client_id, client_secret, code, storeId)
+              const bling = await blingAuth(client_id, client_secret, code, storeId)
               bling.get('/categorias/lojas')
                 .then(() => {
                   console.log('deu certo a request de autenticação')
