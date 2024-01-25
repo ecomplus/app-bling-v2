744d8880172b14a56640fef1d03ea3526345278d
matheusgnreis <matheus@e-com.club>
2024-01-24 23:26:13 -0300

    chore: set access

--- functions/lib/bling-auth/renovate-token.js
+++ functions/lib/bling-auth/renovate-token.js
@@ -7,36 +7,36 @@ module.exports = async () => {
   const maxDocs = 15
   const now = Timestamp.now().toMillis()
 
-  const handleAuth = (clientId, clientSecret, code = undefined, storeId, refreshToken) => {
+  const handleAuth = async (clientId, clientSecret, code = undefined, storeId, refreshToken) => {
     console.log('> Bling Auth02 ', storeId)
-    return auth(clientId, clientSecret, code, storeId, refreshToken)
-      .then((data) => {
-        console.log('> Bling token => ', JSON.stringify(data))
-        if (documentRef) {
-          return documentRef.set({
-            ...data,
-            storeId,
-            clientId,
-            clientSecret,
-            updatedAt: Timestamp.fromMillis(now),
-            expiredAt: Timestamp.fromMillis(now + ((res.data.expires_in - 300) * 1000))
-          }).catch(console.error)
-        }
-      })
+    const data = await auth(clientId, clientSecret, code, storeId, refreshToken)
+    console.log('> Bling token => ', JSON.stringify(data))
+    if (documentRef) {
+      return documentRef.set({
+        ...data,
+        storeId,
+        clientId,
+        clientSecret,
+        updatedAt: Timestamp.fromMillis(now),
+        expiredAt: Timestamp.fromMillis(now + ((res.data.expires_in - 300) * 1000))
+      }).catch(console.error)
+    }
   }
 
   if (firestoreColl) {
     const db = getFirestore()
     const d = new Date(new Date().getTime() + 7200000)
     const documentSnapshot = await db.collection(firestoreColl)
-      .where('updatedAt', '<=', d)
-      .orderBy('updatedAt')
+    console.log('data', d)
+      .where('expiredAt', '<=', d)
+      .orderBy('expiredAt')
       .get()
     const { docs } = documentSnapshot
+    console.log(`There is ${docs && docs.length} docs expiring in two hours`)
     const maxExistedDocs = docs && docs.length > maxDocs 
       ? maxDocs
       : (docs && docs.length) || 0
-    console.log(`There is ${docs && docs.length} docs expiring in two hours`)
+    console.log('max existed docs', maxExistedDocs)
     if (maxExistedDocs) {
       for (let i = 0; i < maxExistedDocs; i++) {
         const doc = docs[i].data();
@@ -49,7 +49,7 @@ module.exports = async () => {
               documentRef.get()
                 .then((documentSnapshot) => {
                   const hasToCreateOauth = (documentSnapshot.exists &&
-                    (now + 1000 * 60 * 60 * 2 + 1000 * 60 * 10 < updatedAt.toMillis()))
+                    (now + 1000 * 60 * 60 * 2 + 1000 * 60 * 10 < expiredAt.toMillis()))
                   if (!hasToCreateOauth) {
                     handleAuth(clientId, clientSecret, code = undefined, storeId, documentSnapshot.get('refresh_token'))
                   }
