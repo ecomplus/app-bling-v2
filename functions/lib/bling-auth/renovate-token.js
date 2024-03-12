48bbe5d6d1708c26c32ed6785df7fc9196ac854f
Wisley Alves <wisley7lgithub@gmail.com>
2024-03-12 09:09:36 -0300

    fix(auth): retry renovate token

--- functions/lib/bling-auth/renovate-token.js
+++ functions/lib/bling-auth/renovate-token.js
@@ -4,36 +4,47 @@ const { getFirestore, Timestamp } = require('firebase-admin/firestore')
 const firestoreColl = 'bling_tokens'
 const lastTimeUpdate = 2 * 60 * 60 * 1000
 
+const delay = (timeout = 200) => new Promise(resolve => setTimeout(resolve(true), timeout))
+
 module.exports = async () => {
   const maxDocs = 15
   const now = Timestamp.now()
 
-  const handleAuth = async (document) => {
-    const doc = document.data()
-    const {
-      clientId,
-      clientSecret,
-      refresh_token: refreshToken,
-      storeId
-    } = doc
-    console.log('> Renove Token Bling ', storeId)
-    const data = await auth(clientId, clientSecret, null, storeId, refreshToken)
-    console.log('> Bling new token => ', JSON.stringify(data))
-    if (document.ref) {
-      return document.ref.set(
-        {
-          ...data,
-          updatedAt: now,
-          expiredAt: Timestamp.fromMillis(now.toMillis() + lastTimeUpdate)
-        },
-        { merge: true }
-      ).catch(console.error)
+  const handleAuth = async (document, isRetry) => {
+    try {
+      const doc = document.data()
+      const {
+        clientId,
+        clientSecret,
+        refresh_token: refreshToken,
+        storeId
+      } = doc
+      console.log('> Renove Token Bling ', storeId)
+      const data = await auth(clientId, clientSecret, null, storeId, refreshToken)
+      console.log('> Bling new token => ', JSON.stringify(data))
+      if (document.ref) {
+        return document.ref.set(
+          {
+            ...data,
+            updatedAt: now,
+            expiredAt: Timestamp.fromMillis(now.toMillis() + lastTimeUpdate)
+          },
+          { merge: true }
+        ).catch(console.error)
+      }
+    } catch (e) {
+      if (!isRetry) {
+        await delay()
+        return handleAuth(document, true)
+      } else {
+        throw e
+      }
     }
   }
 
   if (firestoreColl) {
     const db = getFirestore()
-    const lastDateUpdate = Timestamp.fromMillis(now.toMillis() - 0.5 * lastTimeUpdate) // Discard probable deactivateDd tokens
+    const lastDateUpdate = Timestamp.fromMillis(now.toMillis() - 2 * lastTimeUpdate) // Discard probable deactivateDd tokens
     const nextDateUpdate = Timestamp.fromMillis(now.toMillis() + lastTimeUpdate)
     console.log('> ExpiredAt >=', lastDateUpdate.toDate(), ' < ', nextDateUpdate.toDate())
     const documentSnapshot = await db.collection(firestoreColl)
