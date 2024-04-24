2a4f4abedadc12b4a76da50acc935712f1530cb8
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-04-24 13:23:43 -0300

    chore(product-to-bling): finish parse product

--- functions/lib/integration/parsers/product-to-bling.js
+++ functions/lib/integration/parsers/product-to-bling.js
@@ -5,12 +5,13 @@ module.exports = (product, originalBlingProduct, blingProductCode, blingStore, a
 
   const blingProduct = {
     nome: product.name || '',
+    codigo: product.sku || product._id,
     tipo: 'P',
     situacao: product.available && product.visible ? 'A' : 'I',
     formato: hasVariations ? 'V' : 'S',
-    codigo: blingProductCode,
     preco: ecomUtils.price(product),
-    descricaoCurta: product.short_description,
+    descricaoCurta: product.body_html || product.short_description,
+    descricaoComplementar: product.short_description,
     unidade: originalBlingProduct && originalBlingProduct.unidade
       ? originalBlingProduct.unidade
       : product.measurement && product.measurement.unit !== 'oz' && product.measurement.unit !== 'ct'
@@ -18,16 +19,6 @@ module.exports = (product, originalBlingProduct, blingProductCode, blingStore, a
         : 'UN'
   }
 
-  // if (product.cost_price) {
-  //   blingProduct.preco_custo = product.cost_price
-  // }
-  // if (!hasVariations) {
-  //   if (typeof product.quantity === 'number') {
-  //     blingProduct.volumes = product.quantity
-  //   } else if (originalBlingProduct) {
-  //     blingProduct.volumes = originalBlingProduct.estoqueAtual
-  //   }
-  // }
   if (product.condition) {
     blingProduct.condicao = 0
     if (product.condition === 'new') {
@@ -51,16 +42,12 @@ module.exports = (product, originalBlingProduct, blingProductCode, blingStore, a
     blingProduct.descricaoCurta = product.name
   }
 
-  // if (product.warranty) {
-  //   const warrantyNum = parseInt(product.warranty)
-  //   if (warrantyNum > 0) {
-  //     blingProduct.garantia = warrantyNum
-  //   }
-  // }
 
-  // if (product.mpn && product.mpn.length) {
-  //   blingProduct.class_fiscal = product.mpn[0]
-  // }
+  if (product.mpn && product.mpn.length) {
+    blingProduct.tributacao = {
+      ncm: product.mpn[0]
+    }
+  }
 
   if (product.gtin && product.gtin.length) {
     blingProduct.gtin = product.gtin[0]
@@ -78,13 +65,16 @@ module.exports = (product, originalBlingProduct, blingProductCode, blingStore, a
       case 'g':
         blingProduct.pesoBruto /= 1000
     }
+    blingProduct.pesoLiquido = blingProduct.pesoBruto
   }
+  let dimensionUnit
   if (product.dimensions) {
     const blingProductDimensoes = {}
     for (const side in product.dimensions) {
       if (product.dimensions[side]) {
         const { value } = product.dimensions[side]
         if (value) {
+          dimensionUnit = product.dimensions[side].unit
           const field = side === 'width'
             ? 'largura'
             : side === 'height' ? 'altura' : 'profundidade'
@@ -93,7 +83,7 @@ module.exports = (product, originalBlingProduct, blingProductCode, blingStore, a
       }
     }
     if (Object.keys(blingProductDimensoes).length) {
-      blingProductDimensoes.unidadeMedida = 1
+      blingProductDimensoes.unidadeMedida = dimensionUnit || 1
       blingProduct.dimensoes = blingProductDimensoes
     }
   }
@@ -101,38 +91,90 @@ module.exports = (product, originalBlingProduct, blingProductCode, blingStore, a
   if (product.brands && product.brands.length) {
     blingProduct.marca = product.brands[0].name
   }
-  // if (product.videos && product.videos.length) {
-  //   blingProduct.midia.videos.url = product.videos[0].url
-  // }
-  // if (product.pictures && product.pictures.length) {
-  //   blingProduct.imagens.externas = []
-  //   product.pictures.forEach(({ zoom, big, normal }) => {
-  //     const img = (zoom || big || normal)
-  //     if (img) {
-  //       blingProduct.imagens.externas.push({ link: img.url })
-  //     }
-  //   })
-  // }
+  if (product.videos && product.videos.length && product.videos[0].url) {
+    blingProduct.midia = {
+      videos: {
+        url: product.videos[0].url
+      }
+    }
+  }
+  if (product.pictures && product.pictures.length) {
+    blingProduct.imagens = {
+      externas = []
+    }
+    product.pictures.forEach(({ zoom, big, normal }) => {
+      const img = (zoom || big || normal)
+      if (img) {
+        blingProduct.imagens.externas.push({ link: img.url })
+      }
+    })
+  }
 
   if (hasVariations) {
     blingProduct.variacoes = []
 
     product.variations.forEach((variation, i) => {
       const blingVariation = {
-        nome: '',
+        nome: variation.name,
+        variacao: {},
         tipo: 'P',
         situacao: product.available && product.visible ? 'A' : 'I',
         formato: 'S',
         codigo: variation.sku || `${product.sku}-${(i + 1)}`,
         preco: ecomUtils.price({ ...product, ...variation })
-        // estoque: variation.quantity || 0
       }
-      if (appData.bling_deposit) {
-        blingVariation.deposito = {
-          id: appData.bling_deposit,
-          estoque: variation.quantity || 0
+
+      if (variation.mpn && variation.mpn.length) {
+        blingVariation.tributacao = {
+          ncm: variation.mpn[0]
         }
-        delete blingVariation.estoque
+      }
+    
+      if (variation.gtin && variation.gtin.length) {
+        blingVariation.gtin = variation.gtin[0]
+        if (variation.gtin[1]) {
+          blingVariation.gtinEmbalagem = variation.gtin[1]
+        }
+      }
+    
+      if (variation.weight && variation.weight.value) {
+        blingVariation.pesoBruto = variation.weight.value
+        switch (variation.weight.unit) {
+          case 'mg':
+            blingVariation.pesoBruto /= 1000000
+            break
+          case 'g':
+            blingVariation.pesoBruto /= 1000
+        }
+        blingVariation.pesoLiquido = blingVariation
+        blingVariation.pesoBruto
+      }
+      let variationDimensionUnit
+
+      if (variation.dimensions) {
+        const blingVationDimensoes = {}
+        for (const side in variation.dimensions) {
+          if (variation.dimensions[side]) {
+            const { value } = variation.dimensions[side]
+            if (value) {
+              variationDimensionUnit = variation.dimensions[side].unit
+              const field = side === 'width'
+                ? 'largura'
+                : side === 'height' ? 'altura' : 'profundidade'
+                blingVationDimensoes[field] = value
+            }
+          }
+        }
+        if (Object.keys(blingVationDimensoes).length) {
+          blingVationDimensoes.unidadeMedida = variationDimensionUnit || 1
+          blingVariation.dimensoes = blingVationDimensoes
+        }
+      }
+
+      if (!variation.dimensions) {
+        blingVariation.produtoPai = {
+          cloneInfo: true
+        }
       }
 
       for (const gridId in variation.specifications) {
@@ -157,12 +199,13 @@ module.exports = (product, originalBlingProduct, blingProductCode, blingStore, a
                 gridTitle = gridId.charAt(0).toUpperCase() + gridId.slice(1).replace('_', ' ')
             }
             if (blingVariation.nome) {
-              blingVariation.nome += ';'
+              blingVariation.variacao.nome += ';'
+              blingProduct.variation.ordem = i + 1
               if (i > 0) {
                 gridTitle += i === 1 ? ' secundária' : ` ${(i + 1)}`
               }
             }
-            blingVariation.nome += `${gridTitle}:${text.replace(/[:;]/g, '')}`
+            blingVariation.variacao.nome += `${gridTitle}:${text.replace(/[:;]/g, '')}`
           })
         }
       }
